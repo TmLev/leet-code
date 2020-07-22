02ed4a5509520b4dbcf4eb8cf96fff3eebdec7be
TmLev <leva.bugatti@gmail.com>
2020-07-22 15:03:46 +0300

    feat: add 0001-two-sum cpp solution

---
+++ 0001-two-sum/solution.cpp
@@ -0,0 +1,18 @@
+#include <unordered_map>
+#include <vector>
+
+auto twoSum(const std::vector<int>& nums, int target) -> std::vector<int> {
+    std::unordered_map<int, int> met;
+    
+    for (int i = 0; i < nums.size(); ++i) {
+        auto diff = target - nums[i];
+        
+        if (met.find(diff) != met.end()) {
+            return {met[diff], i};
+        }
+        
+        met[nums[i]] = i;
+    }
+    
+    throw std::runtime_error("No solution found");
+}
