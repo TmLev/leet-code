52c50ad96f76ad0c6d9c856074fbbcfe36597aae
TmLev <leva.bugatti@gmail.com>
2020-07-22 18:37:05 +0300

    feat: add 0013-roman-to-integer cpp solution

---
+++ 0013-roman-to-integer/solution.cpp
@@ -0,0 +1,33 @@
+#include <string>
+
+int romanToInt(std::string s) {
+    int result = s.empty() ? 0 : convert(s.back());
+    
+    for (std::size_t i = 0; i + 1 < s.size(); ++i) {
+        int sign = convert(s[i]) < convert(s[i + 1]) ? -1 : 1;
+        result += sign * convert(s[i]);
+    }
+    
+    return result;
+}
+
+constexpr int convert(char c) {
+    switch (c) {
+    case 'I':
+        return 1;
+    case 'V':
+        return 5;
+    case 'X':
+        return 10;
+    case 'L':
+        return 50;
+    case 'C':
+        return 100;
+    case 'D':
+        return 500;
+    case 'M':
+        return 1000;
+    default:
+        throwruntime_error("Unknown Roman character");
+    }
+} 
